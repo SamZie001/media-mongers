eefb6cfa80746d912faafd6275e14bf32e0da323
SamZie001 <samzie001@gmail.com>
2023-06-22 21:22:19 +0100

    stewpeed

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -6,12 +6,13 @@ import bcrypt from "bcrypt";
 export const POST = async (request: Request | NextRequest) => {
   const { username, email, password } = await request.json();
   const salt = await bcrypt.genSalt(12);
+
   await connect();
 
-  // @ts-ignore
   const hashedPwd = await bcrypt.hash(password, salt);
+  const newUser = new User({ username, email, password: hashedPwd });
   try {
-    await User.create({ username, email, password: hashedPwd });
+    await newUser.save();
     return new NextResponse("Account has been created", { status: 201 });
   } catch (error: Error | any) {
     return new NextResponse(error.message, { status: 400 });
